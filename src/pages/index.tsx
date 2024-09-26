422d5a96c44c9fedf4b9571231fdf296c082335c
Yasin Zahiri <yasin.zahiri10@gmail.com>
2024-09-26 20:29:02 +0330

    (PostDetails): added new route for post details page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from "./login/Login";
-import Dashboard from "./dashboard/dashboard";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "@/graphql/client";
+import PostDetails from "./spaces/PostDetails";
+import Dashboard from "./dashboard/Dashboard";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/spaces/:spacename/:postid" element={<PostDetails />} />
       </Routes>
     </ApolloProvider>
   );
