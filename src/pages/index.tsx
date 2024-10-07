2286c65ee4abaea7ad2f4cb73850dd46dcc8c6e1
Yasin Zahiri <yasin.zahiri10@gmail.com>
2024-10-07 05:10:06 +0330

    (routing): removed private route on client side

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,36 +6,20 @@ import { ApolloProvider } from "@apollo/client";
 import PostDetails from "./dashboard/[postid]/PostDetails";
 import NotFound from "../components/NotFound";
 import Dashboard from "./dashboard/dashboard";
-import PrivateRoute from "@/components/PrivateRoute";
 import UnderConstruction from "@/components/UnderConstruction";
 
-function App({ apolloClient }) {
+function App({ apolloClient }: any) {
   return (
     <ApolloProvider client={apolloClient}>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/dashboard/:postid" element={<PostDetails />} />
-        <Route
-          path="/dashboard/forum"
-          element={<PrivateRoute element={UnderConstruction} />}
-        />
-        <Route
-          path="/dashboard/directory"
-          element={<PrivateRoute element={UnderConstruction} />}
-        />
-        <Route
-          path="/dashboard/notification"
-          element={<PrivateRoute element={UnderConstruction} />}
-        />
-        <Route
-          path="/dashboard/message"
-          element={<PrivateRoute element={UnderConstruction} />}
-        />
-        <Route
-          path="/logout"
-          element={<PrivateRoute element={UnderConstruction} />}
-        />
+        <Route path="/dashboard/forum" element={<UnderConstruction />} />
+        <Route path="/dashboard/directory" element={<UnderConstruction />} />
+        <Route path="/dashboard/notification" element={<UnderConstruction />} />
+        <Route path="/dashboard/message" element={<UnderConstruction />} />
+        <Route path="/logout" element={<UnderConstruction />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </ApolloProvider>
